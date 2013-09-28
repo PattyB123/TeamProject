70d478a8979e425b9643adcdbc9f7c5c99dd72c3
Patrick Burke <pat.burke1917@gmail.com>
2013-09-28 10:53:50 -0300

    Built base plugin code.

---
+++ TeamProject/src/teamProject/TeamProject.java
@@ -0,0 +1,26 @@
+package teamProject;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TeamProject extends JavaPlugin {
+public final Logger logger = Logger.getLogger("Minecraft");
+static Plugin plugin;
+
+@Override
+public void onEnable() {
+	PluginDescriptionFile config = this.getDescription();
+	this.logger.info(config.getName() + " Version "  + config.getVersion() + " Has been Enabled!");
+}
+
+@Override
+public void onDisable() {
+	PluginDescriptionFile config = this.getDescription();
+	this.logger.info(config.getName() + " has Been Disabled!");
+}
+
+	
+}
